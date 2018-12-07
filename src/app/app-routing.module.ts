ad960e36577ddd0bcbdfe2d312011fac3dc597e8
Mominul Haque Sejan <sejan840@gmail.com>
2018-12-07 18:56:21 +0600

    First upload 07-12-2018

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { PostsComponent } from './posts/posts.component';
+//import { PostsComponent } from './posts/posts.component';
+//import { PostsComponent } from './posts/posts.component';
+const routes: Routes = [
+  {
+    path:'',
+    component:PostsComponent
+  },
+  {
+    path:'details/:id',
+    component: PostsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
